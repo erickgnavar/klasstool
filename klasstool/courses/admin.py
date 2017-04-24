d3070d8eb52524cb250338dbce825cf733e8123e
Erick Navarro <erick@navarro.io>
2017-04-23 19:27:03 -0500

    Add QR code to session public page

--- klasstool/courses/admin.py
+++ klasstool/courses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 
 from .models import Course, Session
 
@@ -10,8 +11,21 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ('owner',)
 
 
+def generate_qr_codes(modeladmin, request, queryset):
+    for session in queryset:
+        data = '{scheme}://{host}{url}'.format(
+            scheme=request.scheme,
+            host=request.get_host(),
+            url=reverse('courses:session-public', kwargs={'session_id': session.id})
+        )
+        session.make_qrcode(data)
+
+
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'name', 'date', 'is_active')
     list_filter = ('course',)
+    actions = [
+        generate_qr_codes,
+    ]
